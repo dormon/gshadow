5d490867787015264e721232a4e2ce2fe7762806
dormon <imilet@fit.vutbr.cz>
2015-05-14 20:52:45 +0200

    dataDescriptor alokace manager

--- app/dataDescriptor.cpp
+++ app/dataDescriptor.cpp
@@ -2,26 +2,54 @@
 
 using namespace lang;
 
-DataDescriptor::DataDescriptor(Type type){
+DataDescriptor::DataDescriptor(Type type){ 
   this->_type = type;
+}
+DataDescriptor::DataDescriptor(){
+  this->_type = Type::VOID;
 }
 DataDescriptor::Type DataDescriptor::getType(){
   return this->_type;
 }
-IntDescriptor::IntDescriptor      (): DataDescriptor(Type::INT   ){}
-UintDescriptor::UintDescriptor    (): DataDescriptor(Type::UINT  ){}
-FloatDescriptor::FloatDescriptor  (): DataDescriptor(Type::FLOAT ){}
-StringDescriptor::StringDescriptor(): DataDescriptor(Type::STRING){}
+bool DataDescriptor::operator==(DataDescriptor&b){
+  return this->_type==b._type;
+}
 
-ArrayDescriptor::ArrayDescriptor(DataDescriptor&innerType): DataDescriptor(Type::ARRAY){
+IntDescriptor::IntDescriptor      (): DataDescriptor(Type::INT){}
+bool IntDescriptor::operator==(DataDescriptor&b){return this->_type==b.getType();}
+UintDescriptor::UintDescriptor    (): DataDescriptor(Type::UINT){}
+bool UintDescriptor::operator==(DataDescriptor&b){return this->_type==b.getType();}
+FloatDescriptor::FloatDescriptor  (): DataDescriptor(Type::FLOAT){}
+bool FloatDescriptor::operator==(DataDescriptor&b){return this->_type==b.getType();}
+StringDescriptor::StringDescriptor(): DataDescriptor(Type::STRING){}
+bool StringDescriptor::operator==(DataDescriptor&b){return this->_type==b.getType();}
+
+ArrayDescriptor::ArrayDescriptor(
+    DataDescriptor&innerType,
+    unsigned       size     ):DataDescriptor(Type::ARRAY){
   this->_innerType = innerType;
+  this->_size      = size     ;
+}
+bool ArrayDescriptor::operator==(DataDescriptor&b){
+  if(this->_type!=b.getType())return false;
+  return this->_size==((ArrayDescriptor&)b)._size;
 }
 DataDescriptor&ArrayDescriptor::getInnerType(){
   return this->_innerType;
 }
+unsigned ArrayDescriptor::getSize(){
+  return this->_size;
+}
 StructDescriptor::StructDescriptor(): DataDescriptor(Type::STRUCT){}
 void StructDescriptor::addElement(DataDescriptor&descriptor){
   this->_elements.push_back(descriptor);
+}
+bool StructDescriptor::operator==(DataDescriptor&b){
+  if(this->_type!=b.getType())return false;
+  if(this->getNofElements()!=((StructDescriptor&)b).getNofElements())return false;
+  for(unsigned i=0;i<this->getNofElements();++i)
+    if(!(this->getElement(i)==((StructDescriptor&)b).getElement(i)))return false;
+  return true;
 }
 DataDescriptor&StructDescriptor::getElement(unsigned i){
   return this->_elements[i];
@@ -29,10 +57,53 @@ DataDescriptor&StructDescriptor::getElement(unsigned i){
 unsigned StructDescriptor::getNofElements(){
   return this->_elements.size();
 }
-PtrDescriptor::PtrDescriptor(DataDescriptor&innerType): DataDescriptor(Type::PTR){
+PtrDescriptor::PtrDescriptor(
+    DataDescriptor&innerType): DataDescriptor(Type::PTR){
   this->_innerType = innerType;
+}
+bool PtrDescriptor::operator==(DataDescriptor&b){
+  if(this->_type!=b.getType())return false;
+  return this->getInnerType()==((PtrDescriptor&)b).getInnerType();
 }
 DataDescriptor&PtrDescriptor::getInnerType(){
   return this->_innerType;
 }
 
+Data::Data(unsigned type,TypeManager*manager){
+  this->_type    = type   ;
+  this->_manager = manager;
+}
+
+ArrayData::ArrayData(
+    TypeManager*     manager,
+    ArrayDescriptor& descriptor): Data(manager->addType(descriptor),manager){  
+  this->_data = new Data*[descriptor.getSize()];
+  for(unsigned i=0;i<descriptor.getSize();++i)
+    this->_data[i] = this->_manager->allocate(descriptor.getInnerType());
+}
+
+TypeManager::TypeManager(){
+  this->addType(IntDescriptor   ());
+  this->addType(UintDescriptor  ());
+  this->addType(FloatDescriptor ());
+  this->addType(StringDescriptor());
+}
+
+unsigned TypeManager::addType(DataDescriptor descriptor){
+  for(unsigned i=0;i<this->_types.size();++i)
+    if(this->_types[i]==descriptor)return i;
+  this->_types.push_back(descriptor);
+  return this->_types.size()-1;
+}
+
+DataDescriptor&TypeManager::getType(unsigned type){
+  return this->_types[type];
+}
+
+Data*TypeManager::allocate(DataDescriptor&descriptor){
+  if(descriptor.getType()==DataDescriptor::INT)return new BaseData<int        >(this);
+  if(descriptor.getType()==DataDescriptor::INT)return new BaseData<unsigned   >(this);
+  if(descriptor.getType()==DataDescriptor::INT)return new BaseData<float      >(this);
+  if(descriptor.getType()==DataDescriptor::INT)return new BaseData<std::string>(this);
+  return NULL;
+}
--- app/dataDescriptor.h
+++ app/dataDescriptor.h
@@ -3,40 +3,49 @@
 #include"core.h"
 #include<iostream>
 #include<vector>
+#include<typeinfo>
 
 namespace lang{
   class DataDescriptor{
     public:
-      DEF_ENUM(Type,INT,UINT,FLOAT,STRING,ARRAY,STRUCT,PTR);
+      DEF_ENUM(Type,VOID,INT,UINT,FLOAT,STRING,ARRAY,STRUCT,PTR);
     protected:
       Type _type;
     public:
       DataDescriptor(Type type);
-      DataDescriptor(){}
+      DataDescriptor();
       virtual Type getType();
+      virtual bool operator==(DataDescriptor&b);
   };
   class IntDescriptor: public DataDescriptor{
     public:
       IntDescriptor();
+      virtual bool operator==(DataDescriptor&b);
   };
   class UintDescriptor: public DataDescriptor{
     public:
       UintDescriptor();
+      virtual bool operator==(DataDescriptor&b);
   };
   class FloatDescriptor: public DataDescriptor{
     public:
       FloatDescriptor();
+      virtual bool operator==(DataDescriptor&b);
   };
   class StringDescriptor: public DataDescriptor{
     public:
       StringDescriptor();
+      virtual bool operator==(DataDescriptor&b);
   };
   class ArrayDescriptor: public DataDescriptor{
     protected:
       DataDescriptor _innerType;
+      unsigned       _size;
     public:
-      ArrayDescriptor(DataDescriptor&innerType);
+      ArrayDescriptor(DataDescriptor&innerType,unsigned size);
       DataDescriptor& getInnerType();
+      unsigned        getSize();
+      virtual bool operator==(DataDescriptor&b);
   };
   class StructDescriptor: public DataDescriptor{
     protected:
@@ -46,6 +55,7 @@ namespace lang{
       void addElement(DataDescriptor&descriptor);
       DataDescriptor&getElement(unsigned i);
       unsigned getNofElements();
+      virtual bool operator==(DataDescriptor&b);
   };
   class PtrDescriptor: public DataDescriptor{
     protected:
@@ -53,5 +63,51 @@ namespace lang{
     public:
       PtrDescriptor(DataDescriptor&innerType);
       DataDescriptor&getInnerType();
+      virtual bool operator==(DataDescriptor&b);
+  };
+  class TypeManager;
+  class Data{
+    protected:
+      unsigned    _type;
+      TypeManager*_manager;
+    public:
+      Data(unsigned type,TypeManager*manager);
+  };
+  template<typename T>
+  class BaseData: public Data{
+    protected:
+      T _data;
+    public:
+      BaseData(TypeManager*manager,T data=0):
+        Data(0,manager){
+        this->_manager=manager;
+        if(typeid(T)==typeid(int        ))this->_type = this->_manager->addType(IntDescriptor   ());
+        if(typeid(T)==typeid(unsigned   ))this->_type = this->_manager->addType(UintDescriptor  ());
+        if(typeid(T)==typeid(float      ))this->_type = this->_manager->addType(FloatDescriptor ());
+        if(typeid(T)==typeid(std::string))this->_type = this->_manager->addType(StringDescriptor());
+        this->_data=data;
+      }
+      T getData(){
+        return this->_data;
+      }
+      T*getPtr(){
+        return &this->_data;
+      }
+  };
+  class ArrayData: public Data{
+    protected:
+      Data**_data;
+    public:
+      ArrayData(TypeManager*manager,ArrayDescriptor&descriptor);
+  };
+
+  class TypeManager{
+    protected:
+      std::vector<DataDescriptor>_types;
+    public:
+      TypeManager();
+      unsigned       addType(DataDescriptor descriptor);
+      DataDescriptor&getType(unsigned type);
+      Data*          allocate(DataDescriptor&descriptor);
   };
 }
